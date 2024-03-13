c59c24a256ac7b395971fba3d70f3fdcf9ee308f
harish.dewangan <harish.dewangan@servicenow.com>
2024-03-13 21:59:08 +0530

    REVERTED using -n

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -7,8 +7,4 @@ module.exports = app => {
     router.post("/", UserMiddleware.admin, items.create);
 
     router.get("/", UserMiddleware.user, items.findAll);
-
-    //router.get("/", UserMiddleware.user, items.findAll);
-    //router.get("/", UserMiddleware.user, items.findAll);
-    //router.get("/", UserMiddleware.user, items.findAll);
 };
